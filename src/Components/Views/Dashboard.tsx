0bad37b17626fda9861407a1da95a3d1959df289
Roberto-Orazi <robervgg@gmail.com>
2023-07-20 22:01:46 -0300

    Start with formik

--- src/Components/Views/Dashboard.tsx
+++ src/Components/Views/Dashboard.tsx
@@ -2,17 +2,18 @@ import * as React from 'react'
 import { useEffect, useState } from 'react'
 import Box from '@mui/material/Box'
 import { DataGrid, GridColDef, GridCellEditStopParams, GridCellParams } from '@mui/x-data-grid'
-import { Button, styled, Modal, TextField } from '@mui/material'
+import { Formik, FormikHelpers } from 'formik'
+import { Modal, TextField, Button, styled } from '@mui/material'
 import { clearCredentials } from '../../utils/credentials.helper'
 import { useHistory } from 'react-router-dom'
 import { User } from '../../types/types'
-
 
 export const Dashboard = () => {
   const [users, setUsers] = useState<User[]>([])
   const [editedUsers, setEditedUsers] = useState<{ [id: string]: User }>({})
   const [editModalOpen, setEditModalOpen] = useState(false)
   const [selectedUser, setSelectedUser] = useState<User | null>(null)
+  const [addUser, setAddUser] = useState<User | null>(null)
   const history = useHistory()
 
   const logout = () => {
@@ -23,6 +24,20 @@ export const Dashboard = () => {
   useEffect(() => {
     fetchUsers()
   }, [])
+  const validate = (values: FormValues) => {
+    const errors: Partial<FormValues> = {}
+    // Add validation rules for each field if needed
+    if (!values.fullName) {
+      errors.fullName = 'Full Name is required'
+    }
+    if (!values.email) {
+      errors.email = 'Email is required'
+    }
+    if (!values.password) {
+      errors.password = 'Password is required'
+    }
+    return errors
+  }
 
   const fetchUsers = async () => {
     try {
@@ -35,10 +50,20 @@ export const Dashboard = () => {
       const data = await response.json()
       setUsers(data)
     } catch (error) {
-      console.error('Error trayendo los datos de usuario:', error)
+      console.error('Error fetching user data:', error)
     }
   }
+  interface FormValues {
+    fullName: string;
+    email: string;
+    password: string;
+  }
 
+  const initialValues: FormValues = {
+    fullName: '',
+    email: '',
+    password: '',
+  }
   const handleCellEditStop = (params: GridCellEditStopParams) => {
     const { id, field, value } = params
     setEditedUsers((prevEditedUsers) => ({
@@ -47,23 +72,9 @@ export const Dashboard = () => {
     }))
   }
 
-  const handleSaveChanges = async () => {
-    try {
-      const request = Object.entries(editedUsers).map(([id, updatedUser]) =>
-        fetch(`http://localhost:5005/users/${id}`, {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(updatedUser),
-        })
-      )
-      await Promise.all(request)
-      fetchUsers()
-      setEditedUsers({})
-    } catch (error) {
-      console.error('Error guardando los cambios:', error)
-    }
+  const handleAddUser = () => {
+    setAddUser({ id: '', fullName: '', email: '', password: '' })
+    setEditModalOpen(true)
   }
 
   const handleEdit = (user: User) => {
@@ -71,27 +82,65 @@ export const Dashboard = () => {
     setEditModalOpen(true)
   }
 
+  const handleDelete = async (user: User) => {
+    try {
+      const token = '...'
+      await fetch(`http://localhost:5005/users/${user.id}`, {
+        method: 'DELETE',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      fetchUsers()
+    } catch (error) {
+      console.error('Error deleting the user:', error)
+    }
+  }
+
   const handleEditModalClose = () => {
     setSelectedUser(null)
     setEditModalOpen(false)
   }
 
-  const handleEditModalSave = () => {
-    handleEditModalClose()
+  const handleEditModalSave = async (values: Values, formik: FormikHelpers<Values>) => {
+    try {
+      if (selectedUser) {
+        const updatedUser = {
+          ...selectedUser,
+          ...editedUsers[selectedUser.id],
+        }
+        await fetch(`http://localhost:5005/users/${selectedUser.id}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(updatedUser),
+        })
+        fetchUsers()
+        handleEditModalClose()
+      } else {
+        formik.resetForm()
+      }
+    } catch (error) {
+      console.error('Error saving changes:', error)
+    }
   }
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target
-    setSelectedUser((prevUser) => {
-      if (prevUser) {
-        return {
-          ...prevUser,
-          [name]: value,
-        }
-      }
-      return prevUser
-    })
+    setSelectedUser((prevUser) => ({
+      ...prevUser,
+      [name]: value,
+    }))
+    setEditedUsers((prevEditedUsers) => ({
+      ...prevEditedUsers,
+      [selectedUser!.id]: {
+        ...selectedUser!,
+        [name]: value,
+      },
+    }))
   }
+
   const columns: GridColDef[] = [
     { field: 'id', headerName: 'ID', width: 90 },
     {
@@ -115,12 +164,19 @@ export const Dashboard = () => {
     {
       field: 'actions',
       headerName: 'Actions',
-      width: 150,
+      width: 160,
       renderCell: (params: GridCellParams) => (
-        <ButtonEdit onClick={() => handleEdit(params.row)}>Edit</ButtonEdit>
+        <Box sx={{
+          display: 'flex',
+          gap: '1rem'
+        }}>
+          <ButtonEdit onClick={() => handleEdit(params.row)}>Edit</ButtonEdit>
+          <ButtonDelete onClick={() => handleDelete(params.row)}>Del</ButtonDelete>
+        </Box>
       ),
     },
   ]
+
   return (
     <Box sx={{ height: 400, width: '100%' }}>
       <DataGrid
@@ -128,45 +184,57 @@ export const Dashboard = () => {
         columns={columns}
         onCellEditStop={handleCellEditStop}
         checkboxSelection
-        disableRowSelectionOnClick
+        disableSelectionOnClick
         isCellEditable={(params) => params.row.id !== ''}
       />
-      <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '1rem' }}>
-        <ButtonLogout variant="contained" color="primary" onClick={handleSaveChanges}>
-          Save Changes
+      <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '1rem', gap: '1rem' }}>
+        <ButtonLogout variant="contained" color="primary" onClick={handleAddUser}>
+          Add User
         </ButtonLogout>
-        <ButtonLogout onClick={() => logout()}>Logout</ButtonLogout>
+        <ButtonLogout variant="contained" color="primary" onClick={logout}>
+          Logout
+        </ButtonLogout>
       </Box>
-
-      {/* Edit User Modal */}
       <Modal open={editModalOpen} onClose={handleEditModalClose}>
-        <div className="modal-content">
-          <h2>Edit User</h2>
-          {selectedUser && (
-            <>
-              <TextField
-                name="fullName"
-                label="Full Name"
-                value={selectedUser.fullName}
-                onChange={handleInputChange}
-              />
-              <TextField
-                name="email"
-                label="Email"
-                value={selectedUser.email}
-                onChange={handleInputChange}
-              />
-              <TextField
-                name="password"
-                label="Password"
-                value={selectedUser.password}
-                onChange={handleInputChange}
-              />
-              <Button onClick={handleEditModalSave}>Save</Button>
-              <Button onClick={handleEditModalClose}>Cancel</Button>
-            </>
+        <Formik
+          enableReinitialize
+          initialValues={selectedUser || addUser || initialValues}
+          onSubmit={handleEditModalSave}
+          validate={validate}
+        >
+          {({ handleSubmit }) => (
+            <StyledModalBox>
+              <h2>{selectedUser ? 'Edit User' : 'Add User'}</h2>
+              {selectedUser || addUser ? (
+                <>
+                  <TextField
+                    name="fullName"
+                    label="Full Name"
+                    required
+                    onChange={handleInputChange}
+                    value={selectedUser ? selectedUser.fullName : ''}
+                  />
+                  <TextField
+                    name="email"
+                    label="Email"
+                    required
+                    onChange={handleInputChange}
+                    value={selectedUser ? selectedUser.email : ''}
+                  />
+                  <TextField
+                    name="password"
+                    label="Password"
+                    required
+                    onChange={handleInputChange}
+                    value={selectedUser ? selectedUser.password : ''}
+                  />
+                  <Button onClick={handleSubmit}>Save</Button>
+                  <Button onClick={handleEditModalClose}>Cancel</Button>
+                </>
+              ) : null}
+            </StyledModalBox>
           )}
-        </div>
+        </Formik>
       </Modal>
     </Box>
   )
@@ -188,10 +256,27 @@ const ButtonLogout = styled(Button)`
 `
 
 const ButtonEdit = styled(Button)`
-  background-color: #2e7d32;
-  color: white;
+  background-color: #7f9280;
+  color: #000000;
   padding: 0.5rem 1rem;
   font-size: 0.875rem;
   border-radius: 4px;
   text-transform: none;
+`
+
+const ButtonDelete = styled(Button)`
+  background-color: #e42b2b;
+  color: #000000;
+  padding: 0.5rem 1rem;
+  font-size: 0.875rem;
+  border-radius: 4px;
+  text-transform: none;
+`
+
+const StyledModalBox = styled(Box)`
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
 `
